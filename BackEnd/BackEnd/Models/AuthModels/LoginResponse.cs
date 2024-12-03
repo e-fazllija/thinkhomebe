a7bf1f5126884637b7c3309c1a114f1489a0039d
Elsad Fazllija <e.fazllija@kuramasrl.com>
2024-12-03 20:50:17 +0100

    lastname

--- BackEnd/BackEnd/Models/AuthModels/LoginResponse.cs
+++ BackEnd/BackEnd/Models/AuthModels/LoginResponse.cs
@@ -6,7 +6,7 @@ namespace BackEnd.Models.AuthModels
     {
         public string Id { get; set; }
         public string Name { get; set; }
-        public string Lastname { get; set; }
+        public string LastName { get; set; }
         public string Email { get; set; }
         public string Role { get; set; }
         public string Password { get; set; }
