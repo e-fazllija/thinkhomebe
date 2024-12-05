04645bff1ffaf6936479fc4e124316070e4bc697
Elsad Fazllija <e.fazllija@kuramasrl.com>
2024-12-05 14:48:40 +0100

    request

--- BackEnd/BackEnd/Models/RequestModels/RequestUpdateModel.cs
+++ BackEnd/BackEnd/Models/RequestModels/RequestUpdateModel.cs
@@ -5,26 +5,28 @@ namespace BackEnd.Models.RequestModels
 {
     public class RequestUpdateModel
     {
-        public int Id { get; set; } 
+        public int Id { get; set; }
+        public bool Closed { get; set; }
         [Required]
         public int CustomerId { get; set; }
         public virtual Customer Customer { get; set; }
         [Required]
-        public string Contract { get; set; }
+        public string Contract { get; set; } = string.Empty;
         [Required]
-        public string PropertyType { get; set; }
+        public string PropertyType { get; set; } = string.Empty;
         [Required]
-        public string province { get; set; }
+        public string province { get; set; } = string.Empty;
         [Required]
-        public string City { get; set; }
-        public string RoomsNumber { get; set; }
+        public string City { get; set; } = string.Empty;
+        public string? RoomsNumber { get; set; }
         public int MQFrom { get; set; }
         public int MQTo { get; set; }
-        public string PropertyState { get; set; }
-        public string Heating { get; set; }
+        public string? PropertyState { get; set; }
+        public string? Heating { get; set; }
         public int ParkingSpaces { get; set; }
         [Required]
         public double Price { get; set; }
-        public string Notes { get; set; }
+        public string? Notes { get; set; }
+        public bool Archived { get; set; }
     }
 }
