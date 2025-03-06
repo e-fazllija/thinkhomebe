6dfab3dde1666e6357361e9494f4b4a1975d915d
Elsad Fazllija <e.fazllija@kuramasrl.com>
2025-03-06 10:03:26 +0100

    filename nullable

--- BackEnd/BackEnd/Models/InputModels/SendFileModel.cs
+++ BackEnd/BackEnd/Models/InputModels/SendFileModel.cs
@@ -3,7 +3,7 @@
     public class SendFileModel
     {
         public IFormFile File {  get; set; }
-        public string FileName { get; set; }
+        public string? FileName { get; set; }
         public string FolderName { get; set; }
     }
 }
