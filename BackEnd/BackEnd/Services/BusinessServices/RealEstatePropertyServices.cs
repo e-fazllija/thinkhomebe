dd8204c8dd424084908d9abd22e4df661fd7a7bf
Elsad Fazllija <e.fazllija@kuramasrl.com>
2025-04-07 12:12:43 +0200

    fix

--- BackEnd/BackEnd/Services/BusinessServices/RealEstatePropertyServices.cs
+++ BackEnd/BackEnd/Services/BusinessServices/RealEstatePropertyServices.cs
@@ -127,11 +127,11 @@ namespace BackEnd.Services.BusinessServices
                 if (EntityClasses == null)
                     throw new NullReferenceException("Record non trovato!");
 
-                if (EntityClasses.RealEstatePropertyNotes != null && EntityClasses.RealEstatePropertyNotes?.Count > 0)
-                {
-                    _unitOfWork.dbContext.RealEstatePropertyNotes.RemoveRange(EntityClasses.RealEstatePropertyNotes);
-                    await _unitOfWork.SaveAsync();
-                }
+                //if (EntityClasses.RealEstatePropertyNotes != null && EntityClasses.RealEstatePropertyNotes?.Count > 0)
+                //{
+                //    _unitOfWork.dbContext.RealEstatePropertyNotes.RemoveRange(EntityClasses.RealEstatePropertyNotes);
+                //    await _unitOfWork.SaveAsync();
+                //}
 
                 _unitOfWork.RealEstatePropertyRepository.Delete(EntityClasses);
                 await _unitOfWork.SaveAsync();
